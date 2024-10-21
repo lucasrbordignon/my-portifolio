d3f19896a5577afbca2e00edf16689a7b89428cb
Lucas Rodrigues <lucasrodrigues@MacBook-Air-de-Lucas.local>
2024-10-21 11:55:35 -0300

    fix:resposnive

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,12 @@ export default function Home() {
       
       <h2 className="text-2xl font-bold">Bem-vindo ao meu portfólio!</h2>
       <p className="mt-4">
-        Esta é a parte onde você pode adicionar detalhes sobre seus projetos, habilidades e muito mais. 
+      Back in 2012, I decided to try my hand at creating custom Tumblr themes and tumbled head first into the rabbit hole of coding and web development. Fast-forward to today, and I’ve had the privilege of building software for an advertising agency, a start-up, a huge corporation, and a digital product studio.
+
+My main focus these days is building accessible user interfaces for our customers at Klaviyo. I most enjoy building software in the sweet spot where design and engineering meet — things that look good but are also built well under the hood. In my free time, I've also released an online video course that covers everything you need to know to build a web app with the Spotify API.
+
+When I’m not at the computer, I’m usually rock climbing, reading, hanging out with my wife and two cats, or running around Hyrule searching for Korok seeds
+
       </p>
       <div className="mt-8">
         <p>Você pode inserir mais informações sobre seus projetos aqui...</p>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,16 +4,14 @@ const sections = ['sobre', 'projetos', 'experiencia']
 
 export default function Layout({ children }) {
   return (
-    <div className="h-screen flex justify-center">
-      <div className="w-[50%] bg-zinc-800 text-white h-full pt-8 pl-32 relative">
-        <div className="p-16 sticky top-0 flex flex-col justify-center items-start m-auto">
+    <div className="lg:h-screen max-w-[1280px] flex justify-center flex-col lg:flex-row">
+      <div className="lg:w-[50%] pt-8 pl-32">
+        <div className="lg:pt-[15%] flex flex-col justify-center items-start m-auto">
           <h1 className="text-5xl font-semibold text-zinc-100">Lucas Bordignon</h1>
           <h2 className="text-xl  text-zinc-200 my-3">Desenvolvedor Full Stack</h2>
-          <p className="text-zinc-400 text-base text-wrap mb-14">Eu construo experiências digitais com uma ótica diferente para o mundo dos negócios!</p>
+          <p className="text-zinc-400 text-base text-wrap mb-14 w-1/2">Eu construo experiências digitais com uma ótica diferente para o mundo dos negócios!</p>
 
-          <div className="flex gap-3 flex-col w-[50%] justify-center items-start"> 
-
-            
+          <div className="lg:flex gap-3 flex-col w-[50%] justify-center items-start hidden"> 
             {
               sections.map((section) => {
                 return <h1 className='text-zinc-400'>{section}</h1>
@@ -22,15 +20,15 @@ export default function Layout({ children }) {
           </div>
         </div>
 
-        <div  className="flex gap-4 text-center w-[50%] mt-32 items-center justify-start pl-16"> 
+        <div  className="flex gap-4 w-[50%] items-center justify-start lg:fixed lg:bottom-12 relative pb-8"> 
           <AiOutlineInstagram size={32} color='#a1a1aa'/>
           <AiFillLinkedin size={32} color='#a1a1aa'/>
           <AiFillGithub size={32} color='#a1a1aa'/>
         </div>
       </div>
 
-      <div className="w-[50%] overflow-y-auto h-full bg-zinc-800">
-        <div className="p-8">
+      <div className="lg:w-[50%] lg:overflow-y-auto h-full">
+        <div className="lg:pt-[15%] lg:px-12 px-32 lg:pb-12">
           {children}
         </div>
       </div>
